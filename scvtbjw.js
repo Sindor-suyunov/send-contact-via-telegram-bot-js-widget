af8ef24b801c2a5b308a7b8da0b2e24eef2de297
sindor <sindorsuyunov92@gmail.com>
2022-08-03 11:57:46 +0500

    init

---
+++ scvtbjw.js
@@ -0,0 +1,66 @@
+let admins = []; // Array of telegram admins chat_ids
+let bot_token = 'YOUR_TELEGRAM_BOT_TOKEN';
+
+$('.scvtbjw').click(function () {
+    Swal.fire({
+        title: 'Contact Us',
+        html:
+            '<input type="text" id="scvtbjw-input-1" class="swal2-input" placeholder="Phone number">' +
+            '<input type="text" id="scvtbjw-input-2" class="swal2-input" placeholder="Enter your name">',
+        focusConfirm: false,
+        showCancelButton: true,
+        cancelButtonText: 'No, cancel!',
+        reverseButtons: true,
+        preConfirm: () => {
+            let phone = document.getElementById('scvtbjw-input-1').value;
+            let name = document.getElementById('scvtbjw-input-2').value;
+            if (phone === '' || name === '') {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Please fill contact form!',
+                });
+                return false;
+            }
+            let sent = false;
+            let text = `🙎‍♂️  ${name}    ☎️ ${phone}`;
+            for(let i = 0; i < admins.length; i++){
+                $.ajax({
+                    url:'https://api.telegram.org/bot'+bot_token+'/sendMessage',
+                    method:'POST',
+                    data: {
+                        chat_id: admins[i],
+                        text: text,
+                    },
+                    async: false,
+                    success:function(){
+                        sent = true;
+                    },
+                    error:function(){
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Something went wrong!',
+                        })
+                    }
+                });
+            }
+            if (sent){
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Thank you for contacting us, we will contact you as soon as possible ',
+                    showConfirmButton: false,
+                    timer: 3000
+                })
+            }
+        },
+    }).then((result) => {
+        if (result.dismiss === Swal.DismissReason.cancel) {
+            Swal.fire(
+                'Cancelled',
+                '',
+                'success'
+            )
+        }
+    })
+})
